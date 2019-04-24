8f554f6130fe1e19972a6b1aac4bb22cb668d5a6
RCOliveira98 <respondecodigos@gmail.com>
2019-04-23 22:56:30 -0300

    new module aplication

---
+++ src/app/records/entrevista/entrevista.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+// import { EntrevistaModel } from 'app/shared/entrevista.model.ts';
+
+@Component({
+  selector: 'app-entrevista',
+  templateUrl: './entrevista.component.html',
+  styleUrls: ['./entrevista.component.css']
+})
+export class EntrevistaComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
---
+++ src/app/shared/entrevista.model.ts
@@ -0,0 +1,7 @@
+export class EntrevistaModel {
+    id: number;
+    studentName: string;
+    courseName: string;
+    sala: number;
+    questions: Array<string>;
+}
