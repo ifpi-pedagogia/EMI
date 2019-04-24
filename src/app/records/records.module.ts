c11fa7019b0eee84fe9cc412ef7e5adff1eb4cb3
RCOliveira98 <respondecodigos@gmail.com>
2019-04-24 10:42:15 -0300

    Importação do módulo entrevista no records module

--- src/app/records/records.module.ts
+++ src/app/records/records.module.ts
@@ -10,7 +10,7 @@ import { MuralOuthersComponent } from './mural-outhers/mural-outhers.component';
 import { DepositionDetailsComponent } from './deposition-details/deposition-details.component';
 import { Mural2DetailsComponent } from './mural-outhers/mural2-details/mural2-details.component';
 import { Mural3DetailsComponent } from './mural-outhers/mural3-details/mural3-details.component';
-//import { EntrevistaComponent } from './entrevista/entrevista.component';
+import { EntrevistaComponent } from './entrevista/entrevista.component';
 
 @NgModule({
   declarations: [DepositionsComponent, MuralOneComponent, MuralTwoComponent, MuralTreComponent, MuralOuthersComponent, DepositionDetailsComponent, Mural2DetailsComponent, Mural3DetailsComponent, EntrevistaComponent],
