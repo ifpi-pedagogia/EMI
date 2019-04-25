a5fe98c8cfa194e1a2546fe776bce35b6bfbbbb8
RCOliveira98 <respondecodigos@gmail.com>
2019-04-25 15:24:37 -0300

    Definiçao das questoes para a class do component

--- src/app/records/entrevista/entrevista.component.ts
+++ src/app/records/entrevista/entrevista.component.ts
@@ -11,19 +11,19 @@ import { InterviewService } from '../interview.service';
 export class EntrevistaComponent implements OnInit {
 
   interviews: Array<EntrevistaModel>;
-  questions: Array<string>;
+  questionOne: string;
+  questionTwo: string;
+  questionTre: string;
+  questionFor: string;
 
   constructor(private servInterview: InterviewService) { }
 
   ngOnInit() {
     this.interviews = this.servInterview.getInterviews();
-    this.questions[0] = this.servInterview.questionOne;
-    this.questions[1] = this.servInterview.questionTwo;
-    this.questions[2] = this.servInterview.questionTre;
-    this.questions[3] = this.servInterview.questionFor;
-    console.log(this.questions[3]);
-    console.log('SERVICE--> ' + this.servInterview.questionFor);
-    console.log(this.interviews);
+    this.questionOne = 'O que você diria para um estudante de ensino fundamental (09º ano) que está indeciso quanto a qual tipo de ensino médio realizar: O ensino médio não integrado ou o  ensino médio integrado?';
+    this.questionTwo = 'O que você diria a um aluno que acabou de ingressar no 1º ano do ensino médio integrado e está com muitas dúvidas sobre o curso técnico?';
+    this.questionTre = 'Que contribuições o ensino médio integrado está trazendo para sua formação?';
+    this.questionFor = 'Você arrepende-se de ter optado por realizar o curso técnico integrado no Instituto Federal? Justifique.';
   }
 
 }
--- src/app/records/interview.service.ts
+++ src/app/records/interview.service.ts
@@ -8,18 +8,10 @@ import { EntrevistaModel } from './../shared/entrevista.model';
 export class InterviewService {
 
   interviewAll: Array<EntrevistaModel>;
-  questionOne: string;
-  questionTwo: string;
-  questionTre: string;
-  questionFor: string;
 
   constructor() { this.init(); }
 
   private init(): void {
-    this.questionOne = 'O que você diria para um estudante de ensino fundamental (09º ano) que está indeciso quanto a qual tipo de ensino médio realizar: O ensino médio não integrado ou o  ensino médio integrado?';
-    this.questionTwo = 'O que você diria a um aluno que acabou de ingressar no 1º ano do ensino médio integrado e está com muitas dúvidas sobre o curso técnico?';
-    this.questionTre = 'Que contribuições o ensino médio integrado está trazendo para sua formação?';
-    this.questionFor = 'Você arrepende-se de ter optado por realizar o curso técnico integrado no Instituto Federal? Justifique.';
     // console.log('ok--> 1: ' + this.questionOne);
     this.interviewAll = [
       {
