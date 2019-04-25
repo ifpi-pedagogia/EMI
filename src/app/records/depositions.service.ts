e2859026a3e4db62f1a62c60b077e5286239ceb0
RCOliveira98 <respondecodigos@gmail.com>
2019-04-25 15:31:08 -0300

    Add os atributos urlImg e course no model do component deposition

--- src/app/records/depositions.service.ts
+++ src/app/records/depositions.service.ts
@@ -11,82 +11,114 @@ export class DepositionsService {
     {
       id: 1,
       url: 'https://www.youtube.com/embed/myM4kuB3ke8',
-      author: 'Ana Bel e Rafael'
+      author: 'Ana Bel e Rafael',
+      urlImg: '',
+      course: ''
     },
     {
       id: 2,
       url: '',
-      author: 'Ana Luisa'
+      author: 'Ana Luisa',
+      urlImg: '',
+      course: ''
     },
     {
       id: 3,
       url: 'https://www.youtube.com/embed/GWJYuwnCPss',
-      author: 'Anthony'
+      author: 'Anthony',
+      urlImg: '',
+      course: ''
     },
     {
       id: 4,
       url: 'https://www.youtube.com/embed/vtHoOjxLSyk',
-      author: 'Beatriz'
+      author: 'Beatriz',
+      urlImg: '',
+      course: ''
     },
     {
       id: 5,
       url: 'www.youtube.com/embed/mCbY55edrnQ',
-      author: 'Camila Oliveira'
+      author: 'Camila Oliveira',
+      urlImg: '',
+      course: ''
     },
     {
       id: 6,
       url: 'https://www.youtube.com/embed/1GbyUfzVBoI',
-      author: 'Elias'
+      author: 'Elias',
+      urlImg: '',
+      course: ''
     },
     {
       id: 7,
       url: '',
-      author: 'Gian'
+      author: 'Gian',
+      urlImg: '',
+      course: ''
     },
     {
       id: 8,
       url: 'https://www.youtube.com/embed/TIgtY2FGmos',
-      author: 'Juliano'
+      author: 'Juliano',
+      urlImg: '',
+      course: ''
     },
     {
       id: 9,
       url: 'https://www.youtube.com/embed/06ne77ifibw',
-      author: 'Maria Catyane'
+      author: 'Maria Catyane',
+      urlImg: '',
+      course: ''
     },
     {
       id: 10,
       url: 'https://www.youtube.com/embed/Q7imw3bvImc',
-      author: 'Maria Catyane'
+      author: 'Maria Catyane',
+      urlImg: '',
+      course: ''
     },
     {
       id: 11,
       url: 'https://www.youtube.com/embed/goVXAjykDy4',
-      author: 'Renata'
+      author: 'Renata',
+      urlImg: '',
+      course: ''
     },
     {
       id: 12,
       url: '',
-      author: 'Renata Sammer'
+      author: 'Renata Sammer',
+      urlImg: '',
+      course: ''
     },
     {
       id: 13,
       url: 'https://www.youtube.com/embed/u3bX-76FGRE',
-      author: 'Tiago'
+      author: 'Tiago',
+      urlImg: '',
+      course: ''
     },
     {
       id: 14,
       url: '',
-      author: 'Marlon'
+      author: 'Marlon',
+      urlImg: '',
+      course: ''
     },
     {
       id: 15,
       url: '',
-      author: 'Marlon'
+      author: 'Marlon',
+      urlImg: '',
+      course: ''
     },
     {
       id: 16,
       url: 'https://www.youtube.com/embed/zffknHFmlec',
-      author:'Maria Regina'
+      author: 'Maria Regina',
+      urlImg: '',
+      course: ''
     }
     /*
     {
--- src/app/shared/depositions.model.ts
+++ src/app/shared/depositions.model.ts
@@ -2,4 +2,6 @@ export class Depositions {
   public id: number;
   public url: string;
   public author: string;
+  public urlImg: string;
+  public course: string;
 }
