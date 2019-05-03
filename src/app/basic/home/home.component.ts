79458fd1432c938286cddf8f23b094e3fab7f595
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-05-03 14:08:23 -0300

    Adição do item de menu diálogo e ajustes simples no footer

--- src/app/basic/home/home.component.ts
+++ src/app/basic/home/home.component.ts
@@ -7,9 +7,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  public urlVideo: string;
+  public urlFoguete: string;
+  public urlFormando: string;
+  public urlPointer: string;
+  public urlAstronauta: string;
+  public urlMenina1: string;
+  public urlMenina2: string;
+
   constructor() { }
 
   ngOnInit() {
+    this.urlVideo = 'https://www.youtube.com/watch?v=cEXuxwKDZIE&feature=youtu.be';
+    this.urlFoguete = '../../../assets/imagens/rocket.png';
+    this.urlFormando = '../../../assets/imagens/capelo.png';
+    this.urlPointer = '../../../assets/imagens/cursor.png';
+    this.urlAstronauta = '../../../assets/imagens/astronaut.png';
+    this.urlMenina1 = '../../../assets/imagens/intro.png';
+    this.urlMenina2 = '../../../assets/imagens/section2.png';
   }
 
 }
