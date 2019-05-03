d8aa01c6d1cd9df6df00239d595bf70359a7f22a
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-05-03 14:13:17 -0300

    Ajuste de lógica e template dos depoimentos em vídeos

--- src/app/records/depositions.service.ts
+++ src/app/records/depositions.service.ts
@@ -12,63 +12,63 @@ export class DepositionsService {
       id: 1,
       url: 'https://www.youtube.com/embed/myM4kuB3ke8',
       author: 'Ana Bel e Rafael',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento1.jpeg', */ '',
       course: ''
     },
     {
       id: 2,
       url: '',
       author: 'Ana Luisa',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento2.jpeg', */ '',
       course: ''
     },
     {
       id: 3,
       url: 'https://www.youtube.com/embed/GWJYuwnCPss',
       author: 'Anthony',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento3.jpeg', */ '',
       course: ''
     },
     {
       id: 4,
       url: 'https://www.youtube.com/embed/vtHoOjxLSyk',
       author: 'Beatriz',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento4.jpeg', */ '',
       course: ''
     },
     {
       id: 5,
       url: 'www.youtube.com/embed/mCbY55edrnQ',
       author: 'Camila Oliveira',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento5.jpeg', */ '',
       course: ''
     },
     {
       id: 6,
       url: 'https://www.youtube.com/embed/1GbyUfzVBoI',
       author: 'Elias',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento6.jpeg', */ '',
       course: ''
     },
     {
       id: 7,
       url: 'https://www.youtube.com/embed/TIgtY2FGmos',
       author: 'Juliano',
-      urlImg: '',
+      urlImg: /* 'assets/imagens/depoimento7.jpeg', */ '',
       course: ''
     },
     {
       id: 8,
       url: 'https://www.youtube.com/embed/06ne77ifibw',
       author: 'Maria Catyane',
-      urlImg: '',
+      urlImg: '', /* 'assets/imagens/depoimento8.jpeg', */
       course: ''
     },
     {
       id: 9,
       url: 'https://www.youtube.com/embed/Q7imw3bvImc',
       author: 'Maria Catyane',
-      urlImg: '',
+      urlImg: '', /* 'assets/imagens/depoimento9.jpeg', */
       course: ''
     },
     {
