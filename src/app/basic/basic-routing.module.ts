daa9b63505a7980ae22c6fcb9ca89002192050ec
RCOliveira98 <rcoliveira.ti@gmail.com>
2019-06-03 22:35:19 -0300

    Adição e configuração do component sobre.

--- src/app/basic/basic-routing.module.ts
+++ src/app/basic/basic-routing.module.ts
@@ -4,11 +4,13 @@ import { Routes, RouterModule } from '@angular/router';
 import { BriefHistoryComponent } from './brief-history/brief-history.component';
 import { EducationComponent } from './education/education.component';
 import { HomeComponent } from './home/home.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 const routes: Routes = [
   {path: '', component: HomeComponent},
   {path: 'breve-historico', component: BriefHistoryComponent},
-  {path: 'EPI', component: EducationComponent}
+  {path: 'EPI', component: EducationComponent},
+  {path: 'sobre', component: SobreComponent}
 ];
 
 @NgModule({
--- src/app/basic/basic.module.ts
+++ src/app/basic/basic.module.ts
@@ -5,9 +5,10 @@ import { BasicRoutingModule } from './basic-routing.module';
 import { HomeComponent } from './home/home.component';
 import { EducationComponent } from './education/education.component';
 import { BriefHistoryComponent } from './brief-history/brief-history.component';
+import { SobreComponent } from './sobre/sobre.component';
 
 @NgModule({
-  declarations: [HomeComponent, EducationComponent, BriefHistoryComponent],
+  declarations: [HomeComponent, EducationComponent, BriefHistoryComponent, SobreComponent],
   imports: [
     CommonModule,
     BasicRoutingModule
