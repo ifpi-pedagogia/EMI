af7f328386a4318dcc4e0ba1723de16f8d89254a
Romário C. Oliveira <romario.c.oliveira@accenture.com>
2024-08-12 13:05:55 -0300

    fix: update images path

--- src/app/basic/home/home.component.ts
+++ src/app/basic/home/home.component.ts
@@ -19,12 +19,12 @@ export class HomeComponent implements OnInit {
 
   ngOnInit() {
     this.urlVideo = 'https://www.youtube.com/watch?v=cEXuxwKDZIE&feature=youtu.be';
-    this.urlFoguete = '../../../assets/imagens/rocket.png';
-    this.urlFormando = '../../../assets/imagens/capelo.png';
-    this.urlPointer = '../../../assets/imagens/cursor.png';
-    this.urlAstronauta = '../../../assets/imagens/astronaut.png';
-    this.urlMenina1 = '../../../assets/imagens/intro.png';
-    this.urlMenina2 = '../../../assets/imagens/section2.png';
+    this.urlFoguete = 'https://ifpi-pedagogia.github.io/EMI/assets/imagens/rocket.png';
+    this.urlFormando = 'https://ifpi-pedagogia.github.io/EMI/assets/imagens/capelo.png';
+    this.urlPointer = 'https://ifpi-pedagogia.github.io/EMI/assets/imagens/cursor.png';
+    this.urlAstronauta = 'https://ifpi-pedagogia.github.io/EMI/assets/imagens/astronaut.png';
+    this.urlMenina1 = 'https://ifpi-pedagogia.github.io/EMI/assets/imagens/intro.png';
+    this.urlMenina2 = 'https://ifpi-pedagogia.github.io/EMI/assets/imagens/section2.png';
   }
 
 }
